3e93b75f86c60b4bb0a82fbf64a9fed2ad8f9316
AhmadMehdawi <alalmahdawi15@cit.just.edu.jo>
2021-03-07 10:25:05 +0200

    update

--- app/src/main/java/com/example/sanaati/UsersAuth/Activity/SignupActivity.java
+++ app/src/main/java/com/example/sanaati/UsersAuth/Activity/SignupActivity.java
@@ -65,7 +65,7 @@ public class SignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
 
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
