2435479cbeca09681825787c4d521b7092b1f900
mnoor&ideco.com.jo <IdecoPass@007>
2021-04-29 11:37:14 +0300

    up

--- app/src/main/java/com/example/sanaati/Customers/Activity/MainActivity.java
+++ app/src/main/java/com/example/sanaati/Customers/Activity/MainActivity.java
@@ -4,12 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -43,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout homeRel, profileRel;
     FloatingActionButton fab;
     FrameLayout frameLayout;
+    Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +85,14 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+        GPSTracker gpsTracker = new GPSTracker(MainActivity.this);
+        location = gpsTracker.getLocation();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Users").document("type").collection("Customers").document(getSharedPreferences("Info",MODE_PRIVATE).getString("userid",""))
+                .update("location", location.getLatitude()+","+location.getLongitude());
+
 
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -116,6 +135,182 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
     }
+
+    public final class GPSTracker implements LocationListener {
+
+        private final Context mContext;
+
+        // flag for GPS status
+        public boolean isGPSEnabled = false;
+
+        // flag for network status
+        boolean isNetworkEnabled = false;
+
+        // flag for GPS status
+        boolean canGetLocation = false;
+
+        Location location; // location
+        double latitude; // latitude
+        double longitude; // longitude
+
+        // The minimum distance to change Updates in meters
+        private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
+
+        // The minimum time between updates in milliseconds
+        private static final long MIN_TIME_BW_UPDATES = 1; // 1 minute
+
+        // Declaring a Location Manager
+        protected LocationManager locationManager;
+
+        public GPSTracker(Context context) {
+            this.mContext = context;
+            getLocation();
+        }
+
+        public Location getLocation() {
+            try {
+                locationManager = (LocationManager) mContext
+                        .getSystemService(Context.LOCATION_SERVICE);
+
+                // getting GPS status
+                isGPSEnabled = locationManager
+                        .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                Log.v("isGPSEnabled", "=" + isGPSEnabled);
+
+                // getting network status
+                isNetworkEnabled = locationManager
+                        .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+                Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
+
+                if (isGPSEnabled == false && isNetworkEnabled == false) {
+                    // no network provider is enabled
+                } else {
+                    this.canGetLocation = true;
+                    if (isNetworkEnabled) {
+                        location = null;
+                        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                        }
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        Log.d("Network", "Network");
+                        if (locationManager != null) {
+                            location = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                            }
+                        }
+                    }
+                    // if GPS Enabled get lat/long using GPS Services
+                    if (isGPSEnabled) {
+                        location=null;
+                        if (location == null) {
+                            locationManager.requestLocationUpdates(
+                                    LocationManager.GPS_PROVIDER,
+                                    MIN_TIME_BW_UPDATES,
+                                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                            Log.d("GPS Enabled", "GPS Enabled");
+                            if (locationManager != null) {
+                                location = locationManager
+                                        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                                if (location != null) {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                }
+                            }
+                        }
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return location;
+        }
+
+        public void stopUsingGPS() {
+            if (locationManager != null) {
+                locationManager.removeUpdates(GPSTracker.this);
+            }
+        }
+
+        public double getLatitude() {
+            if (location != null) {
+                latitude = location.getLatitude();
+            }
+            return latitude;
+        }
+
+        public double getLongitude() {
+            if (location != null) {
+                longitude = location.getLongitude();
+            }
+
+            // return longitude
+            return longitude;
+        }
+
+        public boolean canGetLocation() {
+            return this.canGetLocation;
+        }
+
+        public void showSettingsAlert() {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+
+            // Setting Dialog Title
+            alertDialog.setTitle("GPS is settings");
+
+            // Setting Dialog Message
+            alertDialog
+                    .setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+            // On pressing Settings button
+            alertDialog.setPositiveButton("Settings",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(
+                                    Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    });
+
+            // on pressing cancel button
+            alertDialog.setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+            // Showing Alert Message
+            alertDialog.show();
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
